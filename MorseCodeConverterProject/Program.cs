aa2c64451ab998b4490f5b3057fbe5aa0e200b20
Matteo DeSimone <thematteod@gmail.com>
2022-09-03 08:58:43 -0400

    Operational

--- MorseCodeConverterProject/Program.cs
+++ MorseCodeConverterProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MorseCodeConverterProject
 {
@@ -6,7 +7,70 @@ namespace MorseCodeConverterProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var morseCode = new Dictionary<string, string>(){
+                {"a",".-"},
+                {"b","-..."},
+                {"c","-.-."},
+                {"d","-.."},
+                {"e","."},
+                {"f","..-."},
+                {"g","--."},
+                {"h","...."},
+                {"i",".."},
+                {"j",".---"},
+                {"k","-.-"},
+                {"l",".-.."},
+                {"m","--"},
+                {"n","-."},
+                {"o","---"},
+                {"p",".--."},
+                {"q","--.-"},
+                {"r",".-."},
+                {"s","..."},
+                {"t","-"},
+                {"u","..-"},
+                {"v","...-"},
+                {"w",".--"},
+                {"x","-..-"},
+                {"y","-.--"},
+                {"z","--.."},
+                {"1",".----"},
+                {"2","..---"},
+                {"3","...--"},
+                {"4","....-"},
+                {"5","....."},
+                {"6","-...."},
+                {"7","--..."},
+                {"8","---.."},
+                {"9","----."},
+                {"0","-----"},
+                {" "," "},
+                {".",".-.-.-"},
+                {",","--..--"},
+                {"?","..--.."},
+                {";","-.-.-."},
+                {":","---..."},
+                {"-","-....-"},
+                {"/","-..-."},
+                {"'",".----."},
+            };
+
+            Console.WriteLine("Welcome to the Morse Code Translater!");
+            Console.WriteLine("Each dot is one measure of time a dash is 3. Leave 3 measures between each letter and 7 measures between words.");
+            Console.WriteLine("Type what you would like translated:");
+
+            var untranslated = Console.ReadLine().ToLower();
+            var translated = new List<string>();
+
+            foreach (char c in untranslated)
+            {
+                translated.Add(morseCode[$"{c}"]);
+            }
+
+            foreach (string letter in translated)
+            {
+                Console.WriteLine($"{letter}");
+            }
         }
     }
 }
